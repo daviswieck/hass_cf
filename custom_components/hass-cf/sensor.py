9761217ce5770f0a03a9312b61e1fc385a5d8975
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2022-08-30 14:38:41 -0500

    Update sensor.py

--- custom_components/hass-cf/sensor.py
+++ custom_components/hass-cf/sensor.py
@@ -1,4 +1,4 @@
 import logging
 from datetime import timedelta, datetime as dt
-from pynhl import Schedule, Scoring, Linescore, Broadcasts
+from espn-api import Schedule, Scoring, Linescore, Broadcasts
 import voluptuous as vol
